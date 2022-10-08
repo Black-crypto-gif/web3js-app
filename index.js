75a9b3f45af4417c0b7e16b979e2a21ff3394a2c
Black-crypto-gif <80727888+Black-crypto-gif@users.noreply.github.com>
2022-10-08 17:10:31 +0100

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
-// run `node index.js` in the terminal
+const UssdMenu = require('ussd-builder');
+let menu = new UssdMenu();
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// Define menu states
+menu.startState({
+  run: () => {
+    // use menu.con() to send response without terminating session
+    menu.con(
+      'Welcome. Choose option:' + '\n1. Show Balance' + '\n2. Buy Airtime'
+    );
+  },
+  // next object links to next state based on user input
+  next: {
+    1: 'showBalance',
+    2: 'buyAirtime',
+  },
+});
+
+menu.state('showBalance', {
+  run: () => {
+    // fetch balance
+    fetchBalance(menu.args.phoneNumber).then((bal) => {
+      // use menu.end() to send response and terminate session
+      menu.end('Your balance is GHC ' + bal);
+    });
+  },
+});
+
+menu.state('buyAirtime', {
+  run: () => {
+    menu.con('Enter amount:');
+  },
+  next: {
+    // using regex to match user input to next state
+    '*\\d+': 'buyAirtime.amount',
+  },
+});
+
+// nesting states
+menu.state('buyAirtime.amount', {
+  run: () => {
+    // use menu.val to access user input value
+    var amount = Number(menu.val);
+    buyAirtime(menu.args.phoneNumber, amount).then((res) => {
+      menu.end('Airtime bought successfully.');
+    });
+  },
+});
+
+// Registering USSD handler with Express
+
+app.post('/ussd', (req, res) => {
+  menu.run(req.body, (ussdResult) => {
+    res.send(ussdResult);
+  });
+});
